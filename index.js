0cc35b9ee3bbabc59306f3cda38d2fe075cd889d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-07 18:56:48 +0100

    pt2

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
 
 let count = 5;
 
-count = count + 1; // 5 + 1 = 6
 
 count = count + 50;
-console.log(count);
+console.log(count); 
+
+let bonusPoints = 50;
+
+bonusPoints = bonusPoints + 50;
+
+console.log(bonusPoints); 
+
+bonusPoints = bonusPoints - 25;
+
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 70;
+
+console.log(bonusPoints);
