5a8c8f32161b57b7fb4f68575014415d8876abc0
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-07 18:02:38 +0100

    clear

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 
 // document.getElementById("count-el").innerText = 5;
 
-// let firstBatch = 5;
-
-// let secondBatch = 7;
-
-// let count = firstBatch + secondBatch;
-
-// console.log(count);
-
-let myAge = 19;
-
-let humanDogRatio = 7;
-
-let myDogAge = myAge * humanDogRatio;
-
-console.log(myDogAge);
