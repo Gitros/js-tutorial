1f3bc0458105b2c2a92769bc261c3dfa8b6cff70
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 18:12:54 +0100

    function with increment

---
+++ index.js
@@ -0,0 +1,12 @@
+let lapsCompleted = 0;
+
+    function laps() {
+        lapsCompleted = lapsCompleted++;
+    }
+
+    laps();
+    laps();
+    laps();
+    laps();
+
+console.log(lapsCompleted);
