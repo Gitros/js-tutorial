8989c5a483a2213cf3c7513bf8310f2733b78a37
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 21:25:35 +0100

    clear

--- index.js
+++
@@ -1,7 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el");
-
-let name = "Jakub";
-let greetign = "Owczarek";
-
-let fullName = name + ' ' + greetign;
-welcomeEl.innerText = fullName + "😎";
