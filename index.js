e6d2029f715313135a70cd11d6a17ca20ac1f013
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 18:31:48 +0100

    string variable

---
+++ index.js
@@ -0,0 +1,9 @@
+let username = "per";
+
+let message = "You have three new notifications"
+
+console.log(message + ' ' + username);
+
+let messageToUser = message + ' ' + username;
+
+console.log(messageToUser);
