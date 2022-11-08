2cfc8a85c182359e75dc04a87e2034bffc3f5f6c
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 18:36:34 +0100

    strings vs numbers

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let name = "Jakub";
+let name = 42;
 
 let greeting = "Hi my name is";
 
 let myGreeting = greeting + ' ' +  name;
 
 console.log(myGreeting);
+
+let points = 4;
+let bonuspoints = "10"
+
+let totalPoints = points + bonuspoints;
+
+console.log(totalPoints);
