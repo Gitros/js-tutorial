02d497883b95a5bb7512663f7c62058370103f47
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 16:16:25 +0100

    clear

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 
 // document.getElementById("count-el").innerText = 5;
 
-let count = 5;
+//let count = 5;
 
-
-count = count + 50;
-console.log(count); 
-
-let bonusPoints = 50;
-
-bonusPoints = bonusPoints + 50;
-
-console.log(bonusPoints); 
-
-bonusPoints = bonusPoints - 75;
-
-console.log(bonusPoints);
-
-bonusPoints = bonusPoints + 45;
-
-console.log(bonusPoints);
+//console.log(count); 
