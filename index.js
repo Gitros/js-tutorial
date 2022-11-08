d4d0f8a68709c898ccbd89344b731e038003d41f
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-08 21:46:20 +0100

    variables practice

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
-let count = 0;
+let firstName = "Jakub";
+let lastName = "Owczarek";
 
-function increment() {
-    count += 1 
-    countEl.textContent = count
-}
+let fullName = firstName + lastName;
 
-function save() {
-    let countStr = count + ' - ';
-    saveEl.textContent += countStr;
-    console.log(count);
-    count = 0;
-    countEl.textContent = 0;
-}
+console.log(fullName);
