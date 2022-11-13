e64aec12a289412ed9890c41f1862cb6bf685370
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-13 20:13:57 +0100

    clear

--- index.js
+++
@@ -1,9 +0,0 @@
-let age = 110;
-
-if (age < 100) {
-    console.log("not elegible");
-} else if (age === 100) {
-    console.log("here is your birthday card from the king!");
-} else {
-    console.log("not elegible you already have one");
-}
