4adb561a6fdde61f16cec44970700b811fcb5d09
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-16 20:35:38 +0100

    arrays

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const messageEL = document.getElementById("message-el");
 const sumEl = document.querySelector("#sum-el");
 const cardsEl = document.querySelector("#cards-el");
+
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -14,7 +16,7 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "do you want to draw a new card?";
@@ -33,3 +35,18 @@ function newCard() {
     sum += card;
     renderGame();
 }
+
+// arrays ordered lists of items
+
+let featuredPosts = ["Check out my netflix clone", 
+"Here's code for my project",
+"I've just relunched my portfolio"
+];
+
+let experience = ["Chief at restaurant `Piccolo Ropczyce`",
+"ZSAT Ropczyce",
+"driver's license cat. B, Technic Informatic",
+"Using js, CSS, HTML, PHP, MYSQL"
+];
+console.log(experience[1]);
+console.log(experience[2]);
