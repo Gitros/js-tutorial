b55c1b5640eb9f4f969bdc6a136549cf31bae34d
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-17 18:56:27 +0100

    clear

--- index.js
+++ index.js
@@ -57,15 +57,3 @@ function newCard() {
 }
 
 // smth else
-
-let hasCompletedCourse = true;
-let givesCertificate = true;
-
-if (hasCompletedCourse === true && givesCertificate === true) {
-        generateCertificate()
-    
-}
-
-function generateCertificate() {
-    console.log("genereting certificate....");
-}
