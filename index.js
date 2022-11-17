fc8659714ad212f1f753ea5c8d69816c5784b426
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-17 19:03:45 +0100

    is alive true

--- index.js
+++ index.js
@@ -49,10 +49,12 @@ function renderGame() {
 }
 
 function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
     let card = getRandomCard();
     sum += card;
     cards.push(card);
     console.log(cards);
     renderGame();
+}
 }
 
