0da69511acb288a4a7d488775305dd83cc9c5f77
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-17 18:26:44 +0100

    clear

--- index.js
+++ index.js
@@ -45,29 +45,3 @@ function newCard() {
     renderGame();
 }
 
-// smth else
-
-let player1Time = 102;
-let player2Time = 107;
-
-function getFastestRaceTime() {
-    if (player1Time < player2Time){
-        return player1Time;
-    } else if (player2Time < player1Time) {
-        return player2Time;
-    } else {
-        return player1Time;
-    }
-}
-
-function totalRaceTime() {
-    return player1Time + player2Time;
-}
-
-let totalTime = totalRaceTime();
-
-console.log(totalTime);
-
-let fastestRace = getFastestRaceTime();
-
-console.log(fastestRace);
