79d43103ac5db64a6577d184c98dfe5487a43171
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-17 18:58:17 +0100

    logical operator pt2

--- index.js
+++ index.js
@@ -57,3 +57,14 @@ function newCard() {
 }
 
 // smth else
+
+let hasSolvedChallange = false;
+let hasHintsLeft = false;
+
+if (hasSolvedChallange === false && hasHintsLeft === false) {
+    showSolution();
+}
+
+function showSolution() {
+    console.log("Showing the solution....");
+}
