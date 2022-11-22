7e6ed32f5df4c3ef6ca1e34a57cd654bcf3ba3e7
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-22 20:53:50 +0100

    if else if else

--- index.js
+++ index.js
@@ -71,3 +71,16 @@ function newCard() {
 }
 
 // 
+let age = 25;
+
+if(age < 6) {
+    console.log("free");
+} else if(age < 17) {
+    console.log("child discount");
+} else if(age < 26) {
+    console.log("student discount");
+} else if(age < 66) {
+    console.log("full price");
+} else { 
+    console.log("senior citizien discount");
+}
