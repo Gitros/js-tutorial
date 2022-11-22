c4e8c39f0b0f1ff6176e60b2e6eabfaa0d87e046
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-22 20:36:32 +0100

    clear

--- index.js
+++ index.js
@@ -73,14 +73,3 @@ function newCard() {
 
 // objects
 
-let course = {
-    title: "Learn CSS grid for free",
-    lessons: 16,
-    creator: "Per Harald Borgen",
-    length: 63,
-    level: 2,
-    isFree: true,
-    tags: ["html","css"]
-}
-
-console.log(course.length);
