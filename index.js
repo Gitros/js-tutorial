813a9a0d15c63348e61bdbbc1da569dbad4ffc6e
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-22 20:56:53 +0100

    array log in for loop

--- index.js
+++ index.js
@@ -71,3 +71,10 @@ function newCard() {
 }
 
 // 
+
+let largeCountries = ["china","india","usa","indonesia","Pakistan"]
+console.log("The 5 largest countries in the world:");
+for(let i = 0; i < largeCountries.length; i++) {
+
+    console.log("- " + largeCountries[i]);
+}
