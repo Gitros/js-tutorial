3394428ba6626ad02907a4e1689a949a3be53311
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-22 20:40:08 +0100

    pt2

--- index.js
+++ index.js
@@ -73,3 +73,11 @@ function newCard() {
 
 // objects
 
+let  castle = {
+    title: "Live like a king in my castle",
+    price: 190,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/castle2.png"]
+}
+
+console.log(castle.price);
