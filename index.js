7f7c71fc6c9acca1a45e36cce986179ecf2652a0
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-23 20:05:22 +0100

    function + math floor/random

--- index.js
+++ index.js
@@ -72,3 +72,11 @@ function newCard() {
 
 // 
 
+let hands = ["rock", "paper", "scissor"];
+
+function randomHand() {
+let randomIndex = Math.floor(Math.random() * 3);
+return hands[randomIndex]
+} 
+
+console.log(randomHand());
