dd6ba239b094ab552ab173c2c126e7ee4f7e0e98
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-23 20:05:34 +0100

    clear

--- index.js
+++ index.js
@@ -72,11 +72,3 @@ function newCard() {
 
 // 
 
-let hands = ["rock", "paper", "scissor"];
-
-function randomHand() {
-let randomIndex = Math.floor(Math.random() * 3);
-return hands[randomIndex]
-} 
-
-console.log(randomHand());
