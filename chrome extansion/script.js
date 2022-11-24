782efdca906e357660b54a0b106258a602058e64
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-24 21:56:35 +0100

    push

--- chrome extansion/script.js
+++ chrome extansion/script.js
@@ -4,5 +4,6 @@ const myLeads = [];
 const inputEl = document.getElementById("input-el");
 
 inputBtn.addEventListener("click", function(){
+    myLeads.push("www.awesomelead.com")
     console.log("Button clicked");
 })
