852b14c108ccbe85993d30d229c84234ba4cd564
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-24 21:55:08 +0100

    const

--- chrome extansion/script.js
+++ chrome extansion/script.js
@@ -1,5 +1,8 @@
 const inputBtn = document.getElementById("input-btn");
 
+const myLeads = [];
+const inputEl = document.getElementById("input-el");
+
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addeventlistener");
+    console.log("Button clicked");
 })
