0bbcc29b346e9e74aafc48d4fbcddf1560447fc5
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-24 21:52:28 +0100

    clear

--- chrome extansion/script.js
+++ chrome extansion/script.js
@@ -3,11 +3,3 @@ const inputBtn = document.getElementById("input-btn");
 inputBtn.addEventListener("click", function(){
     console.log("Button clicked from addeventlistener");
 })
-
-//
-
-const box = document.getElementById("box");
-
-box.addEventListener("click", function(){
-    console.log("i want to open the box!");
-})
