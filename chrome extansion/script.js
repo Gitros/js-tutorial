a193f02df25cd81b8b6ca5d222596475b5fedcab
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-24 21:49:26 +0100

    box making

--- chrome extansion/script.js
+++ chrome extansion/script.js
@@ -3,3 +3,8 @@ const inputBtn = document.getElementById("input-btn");
 inputBtn.addEventListener("click", function(){
     console.log("Button clicked from addeventlistener");
 })
+
+//
+
+const box = document.getElementById("box");
+
