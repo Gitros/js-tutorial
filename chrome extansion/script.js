e6b874cbeee08a6783f46af17018ee48f0265830
Gitros <31899790+Gitros@users.noreply.github.com>
2022-11-28 19:59:18 +0100

    listItems

--- chrome extansion/script.js
+++ chrome extansion/script.js
@@ -2,7 +2,7 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
-const myLeads = ["aesada4214@fsda"];
+const myLeads = [""];
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
@@ -13,8 +13,12 @@ inputBtn.addEventListener("click", function(){
 const renderLeads = () => {
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>" ;
-        
+    listItems += `
+        <li>
+            <a target = '_blank' href = '${myLeads[i]}'>"
+                 ${myLeads[i]}
+             "</a>
+        </li>` ;
     }
 
     ulEl.innerHTML = listItems; 
